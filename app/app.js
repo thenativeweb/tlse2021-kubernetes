d692315f2027a3668df24655f2b11e28d2463d8e
Golo Roden <golo.roden@thenativeweb.io>
2021-08-23 14:16:41 +0200

    Add second part of examples.

--- app/app.js
+++ app/app.js
@@ -7,14 +7,15 @@ const { processenv } = require('processenv');
 
 const logger = flaschenpost.getLogger();
 
+const message = processenv('MESSAGE', 'Hello Kubernetes!');
+const port = processenv('PORT', 3000);
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   logger.info('Received request.', { method, url });
-  res.end('Hallo Kubernetes!');
+  res.end(message);
 });
-
-const port = processenv('PORT', 3000);
 
 server.listen(port, () => {
   logger.info('Server started.', { port });
